40c90c2ae11a9fe0616e0e2c39def51870c49ec0
Telmo Lousada <telmo.lousada@gmail.com>
2025-01-20 14:52:17 +0000

    Changes made to the file

--- src/question.js
+++ src/question.js
@@ -4,12 +4,12 @@ class Question {
         this.choices = choices;
         this.answer = answer;
         this.difficulty = difficulty;
-        }
     }
 
-shuffleChoices() {
-    for (let i = this.choices.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
-    }
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+        }
+    }    
 }
