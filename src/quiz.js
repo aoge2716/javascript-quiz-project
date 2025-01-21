4a85041da45c60752dcf3d45c55b1fa575a9c67d
Telmo Lousada <telmo.lousada@gmail.com>
2025-01-21 14:39:17 +0000

    Updated code

--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,7 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    constructor (questions, timeLimit, timeRemaining){
+    constructor(questions, timeLimit, timeRemaining) {
         // array of Question objects
         this.questions = questions;
         // numbers
@@ -13,44 +13,52 @@ class Quiz {
         this.currentQuestionIndex = 0;
     };
 
-    getQuestion(){
+    getQuestion() {
         return this.questions[this.currentQuestionIndex];
     };
-    
-    moveToNextQuestion(){
+
+    moveToNextQuestion() {
         this.currentQuestionIndex++;
     };
 
-    shuffleQuestions(){
-        for(let i=this.questions.length-1; i>=0; i--){
-            let rand = Math.floor(Math.random()* i+1);
-            [this.questions[i],this.questions[rand]] = [this.questions[rand],this.questions[i]] 
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i >= 0; i--) {
+            let rand = Math.floor(Math.random() * i + 1);
+            [this.questions[i], this.questions[rand]] = [this.questions[rand], this.questions[i]]
         }
     }
 
-    checkAnswer(answer){
-        if(answer === this.questions[this.currentQuestionIndex].answer){
+    checkAnswer(answer) {
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
             this.correctAnswers++;
         }
     }
 
-    hasEnded(){
-        if(this.currentQuestionIndex < this.questions.length){
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
             return false;
         }
         return true;
     }
 
-filterQuestionsByDifficulty(difficulty){
-    this.questions = this.questions.filter(function (question) {
-        return question.difficulty === difficulty;
-    });
-}
-    averageDifficulty(){
-        let totalDifficulty = 0;
-        this.questions.forEach(question => {
-            totalDifficulty += question.difficulty;
+    filterQuestionsByDifficulty(difficulty) {
+        this.questions = this.questions.filter(function (question) {
+            return question.difficulty === difficulty;
         });
-        return totalDifficulty / this.questions.length;
+    }
+    averageDifficulty() {
+  if (typeof difficulty !== 'number' || difficulty < 1 || difficulty > 3) {
+    return "Invalid difficulty! Please provide a number between 1 and 3.";
+}
+
+const filteredQuestions = this.questions.filter(question => question.difficulty === difficulty);
+
+if (filteredQuestions.length === 0) {
+    return 0;
+}
+
+const totalDifficulty = filteredQuestions.reduce((sum, question) => sum + question.difficulty, 0);
+
+return totalDifficulty / filteredQuestions.length;
     }
 }
